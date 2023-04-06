d8be69ebbe850dfd21ada4dcf900404a6fef3d49
shortytheman <shor7ty@gmail.com>
2023-04-07 01:34:29 +0200

    All rounds saves properly

--- FootballDivision/Football.cs
+++ FootballDivision/Football.cs
@@ -80,6 +80,7 @@ public class Football
         }
     }
 
+    //Bruges ikke lige nu.
     void makeStats()
     {
         Random rnd = new Random();
@@ -98,69 +99,211 @@ public class Football
         }
     }
 
-    
+
     void MakeRoundsForLeague(string league)
     {
-        //try
-        //{
-            Random rnd = new Random();
-            
-            //Initial rounds, 0-21 så altså runde 1 til og med 22
-            for (int i = 0; i < 22; i++)
+        Random rnd = new Random();
+
+        //Initial rounds, 1-22 including the last one.
+        for (int i = 0; i < 22; i++)
+        {
+            var tempTeams = new List<Team>(Teams);
+
+            using (StreamWriter w = new StreamWriter("../../../files/" + league + "/round-" + (i + 1) + ".csv"))
             {
-                var tempTeams = new List<Team>(Teams);
-
-                using (StreamWriter w = new StreamWriter("../../../files/" + league + "/round-" + (i + 1) + ".csv"))
+                for (int j = 0; j < 6; j++)
                 {
-                    for (int j = 0; j < 6; j++)
+                    int randomTeam1 = rnd.Next(tempTeams.Count);
+                    int randomTeam2 = rnd.Next(tempTeams.Count);
+                    while (randomTeam1 == randomTeam2)
                     {
-                        int randomTeam1 = rnd.Next(tempTeams.Count);
-                        int randomTeam2 = rnd.Next(tempTeams.Count);
-                        while (randomTeam1 == randomTeam2)
-                        {
-                            randomTeam2 = rnd.Next(tempTeams.Count);
-                        }
-
-                        int killsTeam1 = rnd.Next(25);
-                        int killsTeam2 = rnd.Next(25);
-                        while (killsTeam1 == killsTeam2)
-                        {
-                            killsTeam2 = rnd.Next(25);
-                        }
-                        w.WriteLine(tempTeams[randomTeam1].Abbreviation + "," + tempTeams[randomTeam2].Abbreviation + "," + killsTeam1 + "," + killsTeam2);
-                        
-                        if (randomTeam1 < randomTeam2){
-                        tempTeams.RemoveAt(randomTeam1);
-                        tempTeams.RemoveAt(randomTeam2 - 1);
-                        }
-                        else
-                        {
-                            tempTeams.RemoveAt(randomTeam1);
-                            tempTeams.RemoveAt(randomTeam2);
-                        }
+                        randomTeam2 = rnd.Next(tempTeams.Count);
                     }
 
+                    int killsTeam1 = rnd.Next(25);
+                    int killsTeam2 = rnd.Next(25);
+                    while (killsTeam1 == killsTeam2)
+                    {
+                        killsTeam2 = rnd.Next(25);
+                    }
+
+                    w.WriteLine(tempTeams[randomTeam1].Abbreviation + ";" + tempTeams[randomTeam2].Abbreviation + ";" +
+                                killsTeam1 + ";" + killsTeam2);
+
+                    if (randomTeam1 < randomTeam2)
+                    {
+                        tempTeams.RemoveAt(randomTeam1);
+                        tempTeams.RemoveAt(randomTeam2 - 1);
+                    }
+                    else
+                    {
+                        tempTeams.RemoveAt(randomTeam1);
+                        tempTeams.RemoveAt(randomTeam2);
+                    }
                 }
             }
-        
+        }
 
-        //Additional Rounds, altså Runde 23 til og med 33
-        /*for (int i = 23; i < 34; i++)
+
+        Dictionary<string, int> score = new Dictionary<string, int>();
+        StreamReader reader = null;
+
+        try
         {
-            using (StreamWriter w = new StreamWriter("../../../files/" + league + "/round-" + i + ".csv"))
+
+            for (int i = 0; i < 22; i++)
             {
-                w.WriteLine();
+                reader = new StreamReader("../../../files/" + league + "/round-" + (i + 1) + ".csv");
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var words = line.Split(";");
+                    if (!score.ContainsKey(words[0]))
+                    {
+                        score.Add(words[0], 0);
+                    }
+
+                    if (!score.ContainsKey(words[1]))
+                    {
+                        score.Add(words[1], 0);
+                    }
+
+                    if (Convert.ToInt32(words[2]) > Convert.ToInt32(words[3]))
+                    {
+                        score[words[0]] = score[words[0]] + 1;
+                    }
+                    else
+                    {
+                        score[words[1]] = score[words[1]] + 1;
+                    }
+                }
             }
         }
-        */
-        
-        //}catch
-        //{
-    //    Console.WriteLine("There is no league with that name... try again with a valid league name!");
-    //}
+        catch (Exception e)
+        {
+            Console.WriteLine("That round doesn't exist. Error: " + e);
+        }
 
+
+        //Print team, number of wins to console.
+        for (int i = 0; i < score.Count; i++)
+        {
+            Console.WriteLine("Key: {0}, Value: {1}",
+                score.ElementAt(i).Key,
+                score.ElementAt(i).Value);
+        }
+
+        List<String> topBracket = new List<string>();
+        List<String> lowerBracket = new List<string>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            //kvp = Key Value Pair
+            var highestWins = score.MaxBy(kvp => kvp.Value);
+            var highestWinsTeam = highestWins.Key;
+            var wins = highestWins.Value;
+            topBracket.Add(highestWinsTeam);
+            score.Remove(highestWinsTeam);
+        }
+
+        foreach (var teams in score)
+        {
+            lowerBracket.Add(teams.Key);
+        }
+
+        Console.WriteLine("Top bracket: " + topBracket.Count);
+        Console.WriteLine("Lower bracket: " + lowerBracket.Count);
+
+        //Additional Rounds, 23-33 including the last one.
+        for (int i = 23; i < 34; i++)
+        {
+            var tempTopBracket = new List<String>(topBracket);
+            
+            Console.WriteLine("Start of loop");
+            using (StreamWriter top = new StreamWriter("../../../files/" + league + "/topBracket/round-" + i + ".csv"))
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Start of top writer");
+                    int randomTeam1 = rnd.Next(tempTopBracket.Count);
+                    int randomTeam2 = rnd.Next(tempTopBracket.Count);
+                    Console.WriteLine("Randomteam1: " + randomTeam1 + " Randomteam2: " + randomTeam2);
+                    while (randomTeam1 == randomTeam2)
+                    {
+                        randomTeam2 = rnd.Next(tempTopBracket.Count);
+                    }
+
+                    int killsTeam1 = rnd.Next(25);
+                    int killsTeam2 = rnd.Next(25);
+                    while (killsTeam1 == killsTeam2)
+                    {
+                        killsTeam2 = rnd.Next(25);
+                    }
+
+                    top.WriteLine(tempTopBracket[randomTeam1] + ";" + tempTopBracket[randomTeam2] + ";" +
+                                  killsTeam1 + ";" + killsTeam2);
+
+                    Console.WriteLine("After top writer");
+
+                    if (randomTeam1 < randomTeam2)
+                    {
+                        tempTopBracket.RemoveAt(randomTeam1);
+                        tempTopBracket.RemoveAt(randomTeam2 - 1);
+                    }
+                    else
+                    {
+                        tempTopBracket.RemoveAt(randomTeam1);
+                        tempTopBracket.RemoveAt(randomTeam2);
+                    }
+                }
+            }
+        }
+
+        for (int i = 23; i < 34; i++)
+        {
+            var tempLowerBracket = new List<String>(lowerBracket);
+            using (StreamWriter lower =
+                   new StreamWriter("../../../files/" + league + "/lowerBracket/round-" + i + ".csv"))
+            {
+                Console.WriteLine("Start of lower writer");
+                for (int j = 0; j < 2; j++)
+                {
+                    int randomTeam1 = rnd.Next(tempLowerBracket.Count);
+                    int randomTeam2 = rnd.Next(tempLowerBracket.Count);
+                    while (randomTeam1 == randomTeam2)
+                    {
+                        randomTeam2 = rnd.Next(tempLowerBracket.Count);
+                    }
+
+                    int killsTeam1 = rnd.Next(25);
+                    int killsTeam2 = rnd.Next(25);
+                    while (killsTeam1 == killsTeam2)
+                    {
+                        killsTeam2 = rnd.Next(25);
+                    }
+
+                    lower.WriteLine(tempLowerBracket[randomTeam1] + ";" + tempLowerBracket[randomTeam2] + ";" +
+                                    killsTeam1 + ";" + killsTeam2);
+
+                    Console.WriteLine("After lower writer");
+
+                    if (randomTeam1 < randomTeam2)
+                    {
+                        tempLowerBracket.RemoveAt(randomTeam1);
+                        tempLowerBracket.RemoveAt(randomTeam2 - 1);
+                    }
+                    else
+                    {
+                        tempLowerBracket.RemoveAt(randomTeam1);
+                        tempLowerBracket.RemoveAt(randomTeam2);
+                    }
+                }
+            }
+        }
     }
 }
+
+
 
 
 //Klasser - Indtil videre kun 3. League, Team og LeagueTeam
